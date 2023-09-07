ef46e7b37d63bee3a80e04aa719dad3a4df81f20
Gino804 <giacomo.p03@gmail.com>
2023-09-07 15:00:48 +0200

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all()->sortByDesc('updated_at');
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $project = new Project();
+        $types = Type::all();
+        return view('admin.projects.create', compact('project', 'types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'title' => 'required|string|max:20',
+                'description' => 'required|string',
+                'image' => 'nullable|file',
+                'type_id' => 'nullable|numeric'
+            ]
+        );
+
+        $data = $request->all();
+        $project = new Project();
+
+        if (array_key_exists('image', $data)) {
+            $img_url = Storage::putFile('project_images', $data['image']);
+            $data['image'] = $img_url;
+        }
+
+        $data['slug'] = Str::slug($data['title'], '-');
+        $project->fill($data);
+        $project->save();
+
+        return to_route('admin.projects.show', $project);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+        return view('admin.projects.edit', compact('project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project)
+    {
+        $request->validate(
+            [
+                'title' => 'required|string|max:20',
+                'description' => 'required|string',
+                'image' => 'nullable|file',
+                'type_id' => 'nullable|numeric'
+            ]
+        );
+
+        $data = $request->all();
+
+        if (array_key_exists('image', $data)) {
+            if ($project->image) Storage::delete($project->image);
+            $img_url = Storage::putFile('project_images', $data['image']);
+            $data['image'] = $img_url;
+        }
+
+        $data['slug'] = Str::slug($data['title'], '-');
+        $project->update($data);
+
+        return to_route('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        if ($project->image) Storage::delete($project->image);
+        $project->delete();
+        return to_route('admin.projects.index');
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Type;
+use Faker\Generator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+
+        $type_ids = Type::pluck('id')->toArray();
+
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->type_id = Arr::random($type_ids);
+            $project->title = $faker->text(20);
+            $project->slug = Str::slug($project->title, '-');
+            $project->description = $faker->paragraphs(5, true);
+            //$project->image = Storage::putFile('project_images', $faker->image(storage_path('app/public/project_images'), 250, 250));
+            $project->save();
+        }
+    }
+}
